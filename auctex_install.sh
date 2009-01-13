c1afa907b0e865c920708be6f31ddf1ef31ad88e
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2009-01-13 04:59:57 +0000

    Continue reorganization

---
+++ auctex_install.sh
@@ -0,0 +1,3 @@
+./configure --prefix="c:/documents and settings/vincent/mes documents/emacs modified/trunk/auctex/" --docdir="c:/docume~1/vincent/mesdoc~1/emacsm~1/trunk/auctex/site-lisp/auctex/doc" --infodir="c:/documents and settings/vincent/mes documents/emacs modified/trunk/auctex/site-lisp/auctex/doc/info/" --without-texmf-dir --with-lispdir="c:/documents and settings/vincent/mes documents/emacs modified/trunk/auctex/site-lisp" --with-emacs="c:/documents and settings/vincent/mes documents/emacs modified/trunk/emacs-22.3/bin/emacs.exe"
+make
+make install
