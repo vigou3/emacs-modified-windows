357cfc8656f602f8dc64bc08a622a09647c01b47
vincent <vincent@5a16c0e7-0f79-4c8a-b882-1e6d53282a5a>
2009-07-20 05:55:34 +0000

    Tag release 22.3-modified-5

---
+++ emacs-modified/windows/tags/emacs-22.3-modified-5/ess_install.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+. ./Makeconf
+
+EMACS=${PREFIX}/bin/emacs.exe
+
+cd ess-*
+
+## Edit Makeconf file for our purposes
+cp -p Makeconf Makeconf.orig
+sed \
+-e "/^DESTDIR/s#/usr/local#$PREFIX#" \
+-e '/^EMACS/s#emacs#'${EMACS}'#' \
+-e '/^LISPDIR/s/share\/emacs\/site-lisp/site-lisp\/ess/' \
+-e '/^ETCDIR/s/share\/emacs\///' \
+-e '/^DOCDIR/s/share\///' Makeconf.orig > Makeconf
+
+## Copy the original Emacs info/dir file to the ess/info directory so
+## that ESS can correctly update the file.
+#mkdir -p ${PREFIX}/ess/info/
+#cp -p ${PREFIX}/emacs-${EMACSVERSION}/info/dir ${PREFIX}/ess/info/
+
+## Build and install
+TMPDIR=$TMP make all
+make install
