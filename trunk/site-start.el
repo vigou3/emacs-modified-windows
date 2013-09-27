1040550e540dac0b1b472db0c9fdc5e5a23b2463
vigou3 <vigou3@5a16c0e7-0f79-4c8a-b882-1e6d53282a5a>
2013-09-27 20:21:09 +0000

    Tag version 24.3-modified-3

---
+++ trunk/site-start.el
@@ -0,0 +1,84 @@
+;;; site-start.el --- Customizations for GNU Emacs on OS X
+
+;; Copyright (C) 2012 Vincent Goulet
+
+;; Author: Vincent Goulet
+
+;; This file is part of GNU Emacs.app Modified
+;; http://vgoulet.act.ulaval.ca/emacs
+
+;; GNU Emacs.app Modified is free software; you can redistribute it
+;; and/or modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+
+;;;
+;;; Nice options to have On by default
+;;;
+(mouse-wheel-mode t)			; activate mouse scrolling
+(global-font-lock-mode t)		; syntax highlighting
+(transient-mark-mode t)			; sane select (mark) mode
+(delete-selection-mode t)		; entry deletes marked text
+(show-paren-mode t)			; match parentheses
+(add-hook 'text-mode-hook 'turn-on-auto-fill) ; wrap long lines in text mode
+
+
+;;;
+;;; ESS
+;;;
+;; Following the "source is real" philosophy put forward by ESS, one
+;; should not need the command history and should not save the
+;; workspace at the end of an R session. Hence, both options are
+;; disabled here.
+(setq-default inferior-R-args "--no-save ")
+
+;; Automagically delete trailing whitespace when saving R script
+;; files. One can add other commands in the ess-mode-hook below.
+(add-hook 'ess-mode-hook
+	  '(lambda()
+	     (add-hook 'write-file-functions
+		       (lambda ()
+                         (ess-nuke-trailing-whitespace)))
+	     (setq ess-nuke-trailing-whitespace-p t)))
+
+;; Path to R executable. Uncomment and edit as needed if R is
+;; installed in such an unusual place that ESS can't find it. (And
+;; then keep updating with each R update!)
+;(setq-default inferior-R-program-name
+;              "c:/program files/r/r-2.7.1/bin/rterm.exe")
+
+
+;;;
+;;; AUCTeX
+;;;
+;; Turn on RefTeX for LaTeX documents. Put further RefTeX
+;; customizations in your .emacs file.
+(add-hook 'LaTeX-mode-hook
+	  (lambda ()
+	    (turn-on-reftex)
+	    (setq reftex-plug-into-AUCTeX t)))
+
+;; Add standard Sweave file extensions to the list of files recognized
+;; by AUCTeX.
+(setq TeX-file-extensions
+      '("Rnw" "rnw" "Snw" "snw" "tex" "sty" "cls" "ltx" "texi" "texinfo" "dtx"))
+
+
+;;;
+;;; SVN
+;;;
+;; Support for the Subversion version control system. Use 'M-x
+;; svn-status RET' on a directory under version control to update,
+;; commit changes, revert files, etc., all within Emacs.
+(add-hook 'svn-log-edit-mode-hook 'turn-off-auto-fill) ; useful option
