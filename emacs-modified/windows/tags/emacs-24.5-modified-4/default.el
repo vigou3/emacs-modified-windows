8abd1d3c9cf28980fc215bc50c91ebe3caa5f6d6
vigou3 <vigou3@5a16c0e7-0f79-4c8a-b882-1e6d53282a5a>
2015-12-11 13:48:59 +0000

    Tag version 24.5-modified-4

---
+++ emacs-modified/windows/tags/emacs-24.5-modified-4/default.el
@@ -0,0 +1,33 @@
+;;; default.el --- Default configuration for GNU Emacs
+;;; Used mainly to load custom extensions.
+;;; (Loaded *after* any user and site configuration files)
+
+;; Copyright (C) 2015 Vincent Goulet
+
+;; Author: Vincent Goulet
+
+;; This file is part of Emacs for Windows Modified
+;; http://vgoulet.act.ulaval.ca/emacs
+
+;; GNU Emacs for Windows Modified is free software; you can
+;; redistribute it and/or modify it under the terms of the GNU General
+;; Public License as published by the Free Software Foundation; either
+;; version 3, or (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;;
+;;; Version number of Emacs Modified
+;;;
+;; Define variable and function 'emacs-modified-version'
+(require 'version-modified)
+
+
