ea69f3f4398d839c045b32886e39b62e46ece9f4
vigou3 <vigou3@5a16c0e7-0f79-4c8a-b882-1e6d53282a5a>
2016-03-30 23:16:14 +0000

    Activation of markdown-mode

--- emacs-modified/windows/trunk/site-start.el
+++ emacs-modified/windows/trunk/site-start.el
@@ -88,6 +88,15 @@
 (require 'poly-R)
 
 ;;;
+;;; markdown-mode
+;;;
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;;;
 ;;; SVN
 ;;;
 ;; Support for the Subversion version control system
